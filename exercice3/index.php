c9f86e379783402804fbd6f5561058e385aa4d8f
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 14:28:56 +0100

    Add files via upload

---
+++ exercice3/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 5</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 3</h1>
+    <?php
+    
+    // array ( ... ) ou [...]
+    $months = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+    
+    echo $months[5];
+    
+    ?>  
+    
+    
+</body>
+</html>
