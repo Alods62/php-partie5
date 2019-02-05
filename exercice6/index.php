97842090da7aaf739520dee86dc2cddc6ccd12d4
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-05 09:21:39 +0100

    Add files via upload

---
+++ exercice6/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 5</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 6</h1>
+    <?php
+    
+    $hautsDeFrance = array (
+        '02' => 'Aisne',
+        '59' => 'Nord',
+        '60' => 'Oise',
+        '62' => 'Pas-de-Calais',
+        '80' => 'Somme'    
+    );
+    
+    echo $hautsDeFrance['59'];
+    
+    ?>  
+    
+    
+</body>
+</html>
