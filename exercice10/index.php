2d4b8383e091c4155e0ad8f70bd8331e7fae3773
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-05 09:25:56 +0100

    Add files via upload

---
+++ exercice10/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 5</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 10</h1>
+    <?php
+    
+    // tableau associatif avec une clé et une valeur $exemple = array ( 'clé => 'valeur' )
+    $hautsDeFrance = array (
+        '02' => 'Aisne',
+        '59' => 'Nord',
+        '60' => 'Oise',
+        '62' => 'Pas-de-Calais',
+        '80' => 'Somme'    
+    );    
+    
+
+    foreach($hautsDeFrance as $cle => $element)
+    {
+    ?>
+    <p>Le département <?= $element ?> a le numéro <?= $cle ?> </p>
+    <?php
+    }
+    ?>
+    
+    
+</body>
+</html>
